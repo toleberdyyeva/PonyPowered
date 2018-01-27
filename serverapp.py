bedd494f69ebaee45ccab045ce110dd204515270
buffalobill571 <bdabylovb@gmail.com>
2018-01-28 00:31:33 +0600

    User widget added

--- serverapp.py
+++ serverapp.py
@@ -38,3 +38,78 @@ def get_current_ip():
         import socket
         ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
         return ip
+
+
+class User(QWidget):
+    first = 1
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        mainlayout = QVBoxLayout()
+        deletegroup = QGroupBox('Delete users')
+        addgroup = QGroupBox('Add users')
+        deletelay = QVBoxLayout()
+        addlay = QVBoxLayout()
+        self.list = QListWidget()
+        self.refreshusers()
+        self.delbtn = QPushButton('Delete user', self)
+        self.delbtn.clicked.connect(self.delete)
+        deletelay.addWidget(self.list)
+        deletelay.addWidget(self.delbtn)
+        deletegroup.setLayout(deletelay)
+        loglay = QGridLayout()
+        loglabel = QLabel('Login: ')
+        self.logline = QLineEdit(self)
+        loglay.addWidget(loglabel, 0, 0)
+        loglay.addWidget(self.logline, 0, 1)
+        passlabel1 = QLabel('Password: ')
+        passlabel2 = QLabel('Repeat password: ')
+        self.passline1 = QLineEdit(self)
+        self.passline1.setEchoMode(QLineEdit.Password)
+        self.passline2 = QLineEdit(self)
+        self.passline2.setEchoMode(QLineEdit.Password)
+        self.addstatus = QLabel()
+        loglay.addWidget(passlabel1, 1, 0)
+        loglay.addWidget(self.passline1, 1, 1)
+        loglay.addWidget(passlabel2, 2, 0)
+        loglay.addWidget(self.passline2, 2, 1)
+        loglay.addWidget(self.addstatus)
+        addlay.addLayout(loglay)
+        addbtn = QPushButton('Add user')
+        addbtn.clicked.connect(self.adduser)
+        addlay.addWidget(addbtn)
+        addgroup.setLayout(addlay)
+        mainlayout.addWidget(deletegroup)
+        mainlayout.addWidget(addgroup)
+        self.setLayout(mainlayout)
+
+    def refreshusers(self):
+        self.list.clear()
+        self.userlist = database.selectall()
+        for i in self.userlist:
+            a = QListWidgetItem(i[0])
+            self.list.addItem(a)
+
+    def delete(self):
+        for i in self.list.selectedItems():
+            a = i.text()
+        database.deletebyname(a)
+        self.refreshusers()
+
+    def adduser(self):
+        password1 = self.passline1.text()
+        password2 = self.passline2.text()
+        if password1 == password2:
+            login = self.logline.text()
+            password = hashlib.md5(password1.encode('utf-8')).hexdigest()
+            database.insertuser(login, password)
+            self.refreshusers()
+            self.passline1.setText('')
+            self.passline2.setText('')
+            self.logline.setText('')
+            self.addstatus.setText("<font color='blue'>User added</font>")
+        else:
+            self.addstatus.setText("<font color='red'>Check password</font>")
