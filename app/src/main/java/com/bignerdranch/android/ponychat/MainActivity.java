753fdd2b701e711213273f1155d3505d1e80a23c
Doskhan <doskhan.doszhanov@is.sdu.edu.kz>
2018-01-27 17:55:37 +0600

    Second commit

--- app/src/main/java/com/bignerdranch/android/ponychat/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/ponychat/MainActivity.java
@@ -1,8 +1,15 @@
 package com.bignerdranch.android.ponychat;
 
 import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button joinBtn=(Button)findViewById(R.id.join_btn);
+        Button createbtn = (Button)findViewById(R.id.create_btn);
+
+        final WifiManager wifiMng = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+
+        joinBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                wifiMng.setWifiEnabled(true);
+            }
+        });
+
 
     }
+
+
+
 }
